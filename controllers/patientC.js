900f69fdc412a13948d7e6da51ad3b6a5ee7c948
Quentin <quentin.rivollat@protonmail.com>
2024-04-18 14:26:39 +0200

    Je sais plus

--- controllers/patientC.js
+++ controllers/patientC.js
@@ -7,54 +7,44 @@ const path = require('path'); // Assurez-vous que cette ligne est décommentée
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // Par exemple
 
-exports.createPatient = (req, res, next) => {
-   
+exports.createPatient = async (req, res) => {
+  try {
+    // Hashage du mot de passe
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hashedPassword) {
-    if (err) {
-      return res.status(500).send("Erreur lors du hashage du mot de passe");
+    // Recherche du docteur
+    const doc = await Doctor.findOne({ 'doctorName': req.body.assignedDoctor });
+    if (!doc) {
+      return res.status(404).send("Erreur : docteur non trouvé !");
     }
+    console.log('Success : docteur trouvé : ' + doc._id);
 
-    Doctor.findOne({ 'doctorName': req.body.assignedDoctor })
-    .then(doc => {
-      if (!doc) {
-        // Gérer le cas où le docteur n'existe pas
-        return res.status(404).send("Erreur : docteur non trouvé !");
-      }
-      // Continuer le traitement si le docteur existe
-      console.log('Success : docteur trouvé : ' + doc._id);
-    })
-    .catch(err => {
-      // Gérer l'erreur de la recherche
-      console.error(err);
-      return res.status(500).send("Erreur lors de la recherche du docteur !");
+    // Création du patient
+    const newPatient = new Patient({
+      patientName: req.body.patientName,
+      password: hashedPassword,
+      email: req.body.email,
+      assignedDoctor: doc._id
     });
-  
 
-  const newpatient = new Patient({
-        patientName: req.body.patientName,
-        password: req.body.password, // Vous devriez hasher les mots de passe avant de les stocker !
-        email: req.body.email,
-        assignedDoctor: doc._id
-      });
-    
-    newpatient.save()
-    .then(patient => {
-      // Création de l'utilisateur associé au docteur
-      const newUser = new User({
-        email: patient.email,
-        password: hashedPassword, // Stocker le même mot de passe hashé
-        profileType: "Patient",
-        assignedDoctor: patient._id,
-      });
+    const savedPatient = await newPatient.save();
 
-      newUser.save()
-        .then(() => res.status(201).json({ message: 'Docteur et utilisateur créés !' }))
-        .catch(error => res.status(400).json({ error: "Erreur lors de la création de l'utilisateur : " + + error.message}));
-    })
-    .catch(err => res.status(500).send("Erreur lors de la création du docteur : " + err.message));
+    // Création de l'utilisateur associé au patient
+    const newUser = new User({
+      email: savedPatient.email,
+      password: hashedPassword,
+      profileType: "Patient",
+      profileId: savedPatient._id, // Lier le patient au User
     });
+
+    await newUser.save();
+    res.status(201).json({ message: 'Patient et utilisateur créés !' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur lors de la création du patient ou de l'utilisateur : " + err.message);
+  }
 };
+
 
 exports.getPatients = (req, res) => {
   Patient.find({}).then(patients => {
