9c95db053402b66d5b112c4b5ec22a2de8d00327
Quentin <quentin.rivollat@protonmail.com>
2024-04-29 14:44:25 +0200

    Change DB URI

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-import express, { json, urlencoded, static as staticServe } from 'express';
+import express, { json, urlencoded, static as staticServe } from "express";
 const app = express();
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 // Base de données
-const mongoDB = 'mongodb://127.0.0.1/ma_base_de_donnees'; // URI de ma DB
+const mongoDB =
+  "mongodb+srv://nohepeh973:b1WLyvrljlLFFzLn@nexthospital.ixqdtf5.mongodb.net/?retryWrites=true&w=majority&appName=NextHospital"; // URI de ma DB
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:')); // En cas d'erreur
+db.on("error", console.error.bind(console, "MongoDB connection error:")); // En cas d'erreur
 
 app.use(json());
-app.use(staticServe('public'));
+app.use(staticServe("public"));
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Routes qui elles-mêmes font appelle à des Controllers
-import patientRoutes from './routes/patientRoute.js';
-import doctorRoutes from './routes/doctorRoute.js';
-import userRoutes from './routes/userRoute.js';
-import consultationRoute from './routes/consultationRoute.js';
-import appointmentRoute from './routes/appointmentRoute.js';
-import documentRoute from './routes/documentRoute.js';
-app.use('/patient', patientRoutes);
-app.use('/doctor', doctorRoutes);
-app.use('/auth', userRoutes);
-app.use('/consultation', consultationRoute);
-app.use('/appointment', appointmentRoute);
-app.use('/document', documentRoute);
+import patientRoutes from "./routes/patientRoute.js";
+import doctorRoutes from "./routes/doctorRoute.js";
+import userRoutes from "./routes/userRoute.js";
+import consultationRoute from "./routes/consultationRoute.js";
+import appointmentRoute from "./routes/appointmentRoute.js";
+import documentRoute from "./routes/documentRoute.js";
+app.use("/patient", patientRoutes);
+app.use("/doctor", doctorRoutes);
+app.use("/auth", userRoutes);
+app.use("/consultation", consultationRoute);
+app.use("/appointment", appointmentRoute);
+app.use("/document", documentRoute);
 
 // Démarrage du serveur
 const PORT = process.env.PORT || 3000;
@@ -44,17 +44,16 @@ app.listen(PORT, () => {
 });
 
 // Liste des choses à faire
-app.get('/todo', function(req, res){
+app.get("/todo", function (req, res) {
   const afaire = {
-    '2': "Documents => créer une DB, le schéma",
-    '3': "Documents => générer aléatoirement des docs",
-    '1': "Appointment => enlever l'index path"
+    2: "Documents => créer une DB, le schéma",
+    3: "Documents => générer aléatoirement des docs",
+    1: "Appointment => enlever l'index path",
   };
   return res.status(202).json(afaire);
-
 });
 
 // Si l'URL n'existe pas
-app.get('*', function(req, res){
-  res.sendFile(join(__dirname, '.', 'views', 'error404.html'));
+app.get("*", function (req, res) {
+  res.sendFile(join(__dirname, ".", "views", "error404.html"));
 });
