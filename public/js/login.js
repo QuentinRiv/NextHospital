2aba7f86dc3cb18eb656df61ad7ce0e4f0dc05ea
QuentinRiv <quentin.rivollat@protonmail.com>
2024-05-01 10:09:41 +0200

    New login

--- public/js/login.js
+++ public/js/login.js
@@ -1,41 +1,16 @@
-// Formulaire de connexion
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+console.log("******************");
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-    // API pour checker le user
-    fetch('/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: email, password: password })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Échec de la connexion');
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.token) {
-            localStorage.setItem('token', data.token); // On garde le token au chaud pour... ? TODO : découvrir si utile
-            // Redirection vers /index après la réussite de l'authentification
-            if (data.profileType == 'Patient') {
-                location.replace('/patient/main');
-            }
-            if (data.profileType == 'Doctor') {
-                location.replace('/doctor/main');
-            }
-            console.error("Not Patient, Not Doctor ???"); // On devrait pas arriver ici
+console.log(container)
 
-        } else {
-            throw new Error('Token non reçu');
-        }
-    })
-    .catch(error => {
-        console.error('Erreur :', error);
-    });
-});
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+})
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+    console.log(container)
+})
